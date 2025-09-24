a80470fb439b2e0684b9791dd72330603a8f8f5a
Darlington Musa Kabba <Businesslegit2023@outlook.com>
2025-09-24 10:55:31 +0100

    Force Vercel redeploy - remove appDir configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App directory is enabled by default in Next.js 13.4+
+  // Updated to remove deprecated appDir configuration
 }
 
 module.exports = nextConfig
